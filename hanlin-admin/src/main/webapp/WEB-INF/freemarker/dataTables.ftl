88ceaaac0c5f23cb33f47656b63e0ca6f4103f5c
xuguojun <504085632@qq.com>
2018-07-18 03:17:13 +0800

    新增admin包

--- hanlin-admin/src/main/webapp/WEB-INF/freemarker/dataTables.ftl
+++ hanlin-admin/src/main/webapp/WEB-INF/freemarker/dataTables.ftl
@@ -63,6 +63,7 @@
 <!-- page specific plugin scripts -->
 <script src="${base}/webstatic/assets/js/jquery.dataTables.min.js"></script>
 <script src="${base}/webstatic/assets/js/jquery.dataTables.bootstrap.min.js"></script>
+<script src="${base}/webstatic/js/page.js"></script>
 <script src="${base}/webstatic/assets/js/dataTables.buttons.min.js"></script>
 <script src="${base}/webstatic/assets/js/buttons.flash.min.js"></script>
 <script src="${base}/webstatic/assets/js/buttons.html5.min.js"></script>
@@ -75,104 +76,23 @@
 	<script type="text/javascript">
         jQuery(function ($) {
             //initiate dataTables plugin
-            var myTable = $('#dynamic-table').DataTable({
-                "bAutoWidth": false,
-                "bProcessing": true,
-                "bServerSide": true,
-                "bStateSave": true,
-                "bFilter": false, //是否启动过滤、搜索功能
-                "bSort": false, //是否启动各个字段的排序功能
-                "sDom": '<""r>t<"pageContainer"lip>',
-                "aLengthMenu": [5, 10, 20], //更改显示记录数选项
-                "iDisplayLength": 5,
+            var myTable = $('#dynamic-table').pageTable({
+                "checkBox":true,
                 "sAjaxSource": "${base}/backHomeManage/getData2.action",
                 "aoColumns": [
                     {
                         "bSortable": false,
                         "sClass": "center",
                         "sDefaultContent": "",
-                        render: function (data, type, row, meta) {
-                            var volText = '<label class="pos-rel"><input type="checkbox" class="ace" value="' + row.userName + '"><span class="lbl"></span></label>';
-                            return volText;
+                        "render": function (data, type, row, meta) {
+                            return '<label class="pos-rel"><input type="checkbox" class="ace" value="' + row.userName + '"><span class="lbl"></span></label>';
                         }
                     },
                     {"bSortable": false, "sClass": "center", "mData": "userName", "sDefaultContent": ""},
                     {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""},
                     {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""}
-                ],
-
-                //服务器端，数据回调处理
-                "fnServerData": function (sSource, aDataSet, fnCallback) {
-                    $.ajax({
-                        "dataType": 'json',
-                        "type": "POST",
-                        "url": sSource,
-                        "data": aDataSet,
-                        "success": fnCallback
-                    });
-                },
-                select: {
-                    style: 'multi'
-                },
-                "oLanguage": { //国际化配置
-                    "sProcessing": "正在获取数据，请稍后...",
-                    "sLengthMenu": "每页 _MENU_ 条",
-                    "sZeroRecords": "没有您要搜索的内容",
-                    "sInfo": "，共 _TOTAL_ 条记录",
-                    "sInfoEmpty": "记录数为0",
-                    "sInfoFiltered": "(全部记录数 _MAX_ 条)",
-                    "sInfoPostFix": "",
-                    "sSearch": "搜索",
-                    "sUrl": "",
-                    "oPaginate": {
-                        "sFirst": "第一页",
-                        "sPrevious": "上一页",
-                        "sNext": "下一页",
-                        "sLast": "最后一页"
-                    }
-                }
+                ]
             });
-            myTable.on('select', function (e, dt, type, index) {
-                if (type === 'row') {
-                    $(myTable.row(index).node()).find('input:checkbox').prop('checked', true);
-                }
-            });
-            myTable.on('deselect', function (e, dt, type, index) {
-                if (type === 'row') {
-                    $(myTable.row(index).node()).find('input:checkbox').prop('checked', false);
-                }
-            });
-
-            /////////////////////////////////
-            //table checkboxes
-            $('th input[type=checkbox], td input[type=checkbox]').prop('checked', false);
-
-            //select/deselect all rows according to table header checkbox
-            $('#dynamic-table > thead > tr > th input[type=checkbox], #dynamic-table_wrapper input[type=checkbox]').eq(0).on('click', function () {
-                var th_checked = this.checked;//checkbox inside "TH" table header
-
-                $('#dynamic-table').find('tbody > tr').each(function () {
-                    var row = this;
-                    if (th_checked) myTable.row(row).select();
-                    else myTable.row(row).deselect();
-                });
-            });
-
-            //select/deselect a row when the checkbox is checked/unchecked
-            $('#dynamic-table').on('click', 'td input[type=checkbox]', function () {
-                var row = $(this).closest('tr').get(0);
-                if (this.checked) myTable.row(row).deselect();
-                else myTable.row(row).select();
-            });
-
-
-            $(document).on('click', '#dynamic-table .dropdown-toggle', function (e) {
-                e.stopImmediatePropagation();
-                e.stopPropagation();
-                e.preventDefault();
-            });
-
-
         })
     </script>
 
--- hanlin-admin/src/main/webapp/webstatic/js/page.js
+++ hanlin-admin/src/main/webapp/webstatic/js/page.js
@@ -1,59 +1,97 @@
-function dataTable(tableId){
-    return $('#dynamic-table').DataTable({
-        "bAutoWidth": false,
-        "bProcessing": true,
-        "bServerSide": true,
-        "bStateSave" : true,
-        "bFilter" : false, //是否启动过滤、搜索功能
-        "bSort" : false, //是否启动各个字段的排序功能
-        "sDom":'t<"pageContainer"lip>',
-        "aLengthMenu": [10, 15, 20,40], //更改显示记录数选项
-        "iDisplayLength": 10,
-        "sAjaxSource": "${base}/backHomeManage/getData2.action",
-        "aoColumns": [
-            {
-                "bSortable": false,
-                "sClass": "center",
-                "sDefaultContent": "",
-                render: function (data, type, row, meta) {
-                    return meta.row + 1 +
-                        meta.settings._iDisplayStart;
-                }
-            },
-            {"bSortable": false, "sClass": "center", "mData": "userName", "sDefaultContent": ""},
-            {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""},
-            {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""}
-        ],
+(function ($) {
+    $.fn.pageTable = function (options) {
+        var defaults = {
+            "bAutoWidth": false,
+            "bProcessing": true,
+            "bServerSide": true,
+            "bStateSave": true,
+            "bFilter": false, //是否启动过滤、搜索功能
+            "bSort": false, //是否启动各个字段的排序功能
+            "sDom": '<""r>t<"pageContainer"lip>',
+            "aLengthMenu": [5, 10, 20], //更改显示记录数选项
+            "iDisplayLength": 5,
+            "sAjaxSource": "${base}/backHomeManage/getData2.action",
+            "aoColumns": [
+                {
+                    "bSortable": false,
+                    "sClass": "center",
+                    "sDefaultContent": "",
+                    render: function (data, type, row, meta) {
+                        var volText = '<label class="pos-rel"><input type="checkbox" class="ace" value="' + row.userName + '"><span class="lbl"></span></label>';
+                        return volText;
+                    }
+                },
+                {"bSortable": false, "sClass": "center", "mData": "userName", "sDefaultContent": ""},
+                {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""},
+                {"bSortable": false, "sClass": "center", "mData": "userAge", "sDefaultContent": ""}
+            ],
 
-        //服务器端，数据回调处理
-        "fnServerData": function (sSource, aDataSet, fnCallback) {
-            $.ajax({
-                "dataType": 'json',
-                "type": "POST",
-                "url": sSource,
-                "data": aDataSet,
-                "success": fnCallback
-            });
-        },
-        select: {
-            style: 'multi'
-        },
-        "oLanguage": { //国际化配置
-            "sProcessing" : "正在获取数据，请稍后...",
-            "sLengthMenu" : "每页 _MENU_ 条，",
-            "sZeroRecords" : "没有您要搜索的内容",
-            "sInfo" : "共 _TOTAL_ 条记录",
-            "sInfoEmpty" : "记录数为0",
-            "sInfoFiltered" : "(全部记录数 _MAX_ 条)",
-            "sInfoPostFix" : "",
-            "sSearch" : "搜索",
-            "sUrl" : "",
-            "oPaginate": {
-                "sFirst" : "第一页",
-                "sPrevious" : "上一页",
-                "sNext" : "下一页",
-                "sLast" : "最后一页"
+            //服务器端，数据回调处理
+            "fnServerData": function (sSource, aDataSet, fnCallback) {
+                $.ajax({
+                    "dataType": 'json',
+                    "type": "POST",
+                    "url": sSource,
+                    "data": aDataSet,
+                    "success": fnCallback
+                });
+            },
+            select: {
+                style: 'multi'
+            },
+            "oLanguage": { //国际化配置
+                "sProcessing": "正在获取数据，请稍后...",
+                "sLengthMenu": "每页 _MENU_ 条",
+                "sZeroRecords": "没有您要搜索的内容",
+                "sInfo": "，共 _TOTAL_ 条记录",
+                "sInfoEmpty": "记录数为0",
+                "sInfoFiltered": "(全部记录数 _MAX_ 条)",
+                "sInfoPostFix": "",
+                "sSearch": "搜索",
+                "sUrl": "",
+                "oPaginate": {
+                    "sFirst": "第一页",
+                    "sPrevious": "上一页",
+                    "sNext": "下一页",
+                    "sLast": "最后一页"
+                }
             }
+        };
+        var opts = $.extend({}, defaults, options);
+        var myTable = this.DataTable(opts);
+        if (opts["checkBox"]) {
+            var id = this.attr("id");
+            myTable.on('select', function (e, dt, type, index) {
+                if (type === 'row') {
+                    $(myTable.row(index).node()).find('input:checkbox').prop('checked', true);
+                }
+            });
+            myTable.on('deselect', function (e, dt, type, index) {
+                if (type === 'row') {
+                    $(myTable.row(index).node()).find('input:checkbox').prop('checked', false);
+                }
+            });
+            //table checkboxes
+            $('#' + id + ' > thead > tr > th input[type=checkbox], #' + id + '_wrapper input[type=checkbox]').eq(0).on('click', function () {
+                var th_checked = this.checked;//checkbox inside "TH" table header
+                $('#' + id).find('tbody > tr').each(function () {
+                    var row = this;
+                    if (th_checked) myTable.row(row).select();
+                    else myTable.row(row).deselect();
+                });
+            });
+            //select/deselect a row when the checkbox is checked/unchecked
+            this.on('click', 'td input[type=checkbox]', function () {
+                var row = $(this).closest('tr').get(0);
+                if (this.checked) myTable.row(row).deselect();
+                else myTable.row(row).select();
+            });
+            $(document).on('click', '#' + id + ' .dropdown-toggle', function (e) {
+                e.stopImmediatePropagation();
+                e.stopPropagation();
+                e.preventDefault();
+            });
         }
-    });
-}
+        return myTable;
+    };
+})(jQuery);
